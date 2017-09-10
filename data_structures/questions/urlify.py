e74fdbb21c4d5019160d3372fc5aec0f9347963c
somethingvague <phil.j.regan@gmail.com>
2017-09-10 21:58:55 +0100

    Add solution for 1.3

---
+++ data_structures/questions/urlify.py
@@ -0,0 +1,38 @@
+"""Question 1.3
+Write a method to replace all spaces in a string with '%20'
+"""
+
+
+def custom_urlify(string_list, true_length):
+    """Strips and replaces spaces with '%20' with custom implementation.
+
+    Args:
+        string_list: list with characters to transform
+        true_length: length of the stripped string
+    Returns:
+        string stripped and with strings replaced
+    """
+
+    new_index = len(string_list)
+
+    for i in reversed(range(true_length)):
+        if string_list[i] == " ":
+            string_list[new_index - 3:new_index] = "%20"
+            new_index -= 3
+        else:
+            string_list[new_index - 1] = string_list[i]
+            new_index -= 1
+
+    return string_list[new_index:]
+
+
+def idiomatic_urlify(string_list):
+    """Strips and replaces spaces with '%20' idiomatically.
+
+    Args:
+        string_list: list of characters to transform
+    Returns:
+        string stripped and with strings replaced
+    """
+
+    return list(''.join(string_list).rstrip().replace(" ", "%20"))
---
+++ data_structures/test/test_urlify.py
@@ -0,0 +1,30 @@
+import unittest
+from data_structures.questions.urlify import custom_urlify, idiomatic_urlify
+from data_structures.test.utility import profile
+
+
+class TestURLify(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.string = "Some String With Spaces            "
+        cls.expected = list("Some%20String%20With%20Spaces")
+        cls.true_length = len(''.join(cls.string).rstrip())
+
+    def test_custom_urlify(self):
+        """Tests ``custom_urlify()`` from the urlify module"""
+
+        self.assertEqual(custom_urlify(list(self.string), self.true_length), self.expected)
+
+    def test_idiomatic_urlify(self):
+        """Tests ``idiomatic_urlify()`` from the urlify module"""
+
+        self.assertEqual(idiomatic_urlify(list(self.string)), self.expected)
+
+    def test_execution_times(self):
+        """Tests execution times of implementations"""
+
+        profile("Custom urlify", custom_urlify, list(self.string), self.true_length)
+        profile("Idiomatic urlify", idiomatic_urlify, list(self.string))
+
+if __name__ == "__main__":
+    unittest.main()
