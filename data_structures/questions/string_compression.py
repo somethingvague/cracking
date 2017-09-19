554b3eccd9a10d0a9a091c56fae882a3c7f8cda2
somethingvague <phil.j.regan@gmail.com>
2017-09-19 23:23:53 +0100

    Add solution for 1.6

---
+++ data_structures/questions/string_compression.py
@@ -0,0 +1,35 @@
+"""
+Question 1.6
+Implement a method to perform basic string compression using counts of repeated characters
+"""
+
+
+def compress_string(string):
+    """
+    Compresses string using basic compression in O(N) space and time
+
+    Args:
+        string - to compress
+    Returns:
+        Compressed string or original string if the compressed one is longer
+    """
+
+    count = 0
+    compressed = []
+    current = string[0]
+
+    for letter in string:
+        if letter == current:
+            count += 1
+        else:
+            compressed.extend([current, str(count)])
+            current = letter
+            count = 1
+
+    # Flush the remaining
+    compressed.extend([current, str(count)])
+
+    if len(compressed) >= len(string):
+        return string
+
+    return ''.join(compressed)
---
+++ data_structures/test/test_string_compression.py
@@ -0,0 +1,17 @@
+import unittest
+from data_structures.questions.string_compression import compress_string
+
+
+class TestStringCompression(unittest.TestCase):
+    def test_compress_string(self):
+        self.assertEqual(compress_string("aabcccccaaa"), "a2b1c5a3")
+
+        unique_char_string = "abcdef"
+        self.assertEqual(compress_string(unique_char_string), unique_char_string)
+
+        redundant_compress_string = "aabbccddee"
+        self.assertEqual(compress_string(redundant_compress_string), redundant_compress_string)
+
+
+if __name__ == "__main__":
+    unittest.main()
