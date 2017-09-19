9db66c659eb99c15e988fef96e29a3166c1854d4
somethingvague <phil.j.regan@gmail.com>
2017-09-19 22:06:50 +0100

    add solution and test for 1.5

---
+++ data_structures/questions/one_away.py
@@ -0,0 +1,43 @@
+"""
+Question 1.5
+There are 3 types of edits that can be performed on strings: insert a character, remove a character, replace a
+character. Given two strings, write a function to check if they are one, or zero edits away
+"""
+
+def is_one_away(first, second):
+    """
+    Determines if two strings are one insertion, removal or replacement away from each other in O(N)
+
+    Args:
+        first: string
+        second: string
+    Returns:
+        True if the second is "one away" from the first, False otherwise
+    """
+
+    if abs(len(first)- len(second)) > 1:
+        return False
+
+    # 'Insertion' is the inverse of 'removal'. For these use cases, ensure 'first' is the smaller of the two
+    #  strings so we only need to handle 'insertion'
+    if len(first) > len(second):
+        first, second = second, first
+
+    i = j = differences = 0
+
+    while i < len(first) and i < len(second):
+        if first[i] != second[j]:
+            differences += 1
+            if differences > 1:
+                return False
+
+            # If this is an insertion use case only increment j so we check the next character in 'second' against
+            # the current character in 'first'
+            if len(first) < len(second):
+                j += 1
+                continue
+
+        i += 1
+        j += 1
+
+    return True
---
+++ data_structures/test/test_one_away.py
@@ -0,0 +1,29 @@
+import unittest
+from data_structures.questions.one_away import is_one_away
+
+
+class TestOneAway(unittest.TestCase):
+    def test_is_one_away(self):
+        # Equal
+        self.assertTrue(is_one_away("abc", "abc"))
+
+        # Replacement
+        self.assertTrue(is_one_away("abc", "abx"))
+        self.assertTrue(is_one_away("abc", "asc"))
+
+        # Insertion
+        self.assertTrue(is_one_away("abc", "abcd"))
+        self.assertTrue(is_one_away("abcd", "abxcd"))
+
+        # Removal
+        self.assertTrue(is_one_away("abcd", "acd"))
+        self.assertTrue(is_one_away("abcd", "bcd"))
+
+        # Not One Away
+        self.assertFalse(is_one_away("a", "abc"))
+        self.assertFalse(is_one_away("abc", "axy"))
+        self.assertFalse(is_one_away("abc", "abxy"))
+
+
+if __name__ == "__main__":
+    unittest.main()
