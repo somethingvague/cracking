4c838d39c0c4c3d6af6c4726f1b1568ece8a8e4e
somethingvague <phil.j.regan@gmail.com>
2017-10-07 14:23:11 +0100

    Add solution and test for 1.9

---
+++ data_structures/questions/string_rotation.py
@@ -0,0 +1,26 @@
+"""
+Question 1.9
+Given an is_substring function, write a function which determines if a string is a rotation of another string
+by only calling is_substring once
+"""
+
+
+def is_string_rotation(s1, s2):
+    """
+    Determines if a string is a rotation of another, O(N)
+
+    Args:
+          s1: the string
+          s2: possible rotation
+    Returns:
+          True if s2 is a rotation of s1, False otherwise
+    """
+
+    if len(s1) == len(s2) and is_substring(s1 + s1, s2):
+        return True
+
+    return False
+
+
+def is_substring(s1, s2):
+    return s2 in s1
---
+++ data_structures/test/test_string_rotation.py
@@ -0,0 +1,14 @@
+import unittest
+from data_structures.questions.string_rotation import is_string_rotation
+
+
+class TestStringRotation(unittest.TestCase):
+    def test_string_rotation(self):
+        self.assertTrue(is_string_rotation("waterbottle", "erbottlewat"))
+        self.assertFalse(is_string_rotation("somestring", "stringother"))
+        self.assertFalse(is_string_rotation("somestring", "strsomeing"))
+        self.assertTrue(is_string_rotation("", ""))
+
+
+if __name__ == "__main__":
+    unittest.main()
