3975785f51c51498c5d7e55cd96eff35589cf275
somethingvague <phil.j.regan@gmail.com>
2019-01-23 22:53:23 +0000

    Add solutions to Remove Dupes

---
+++ data_structures/chapter_2/questions/remove_dups.py
@@ -0,0 +1,45 @@
+"""Write code to remove duplicates from an unsorted linked list"""
+
+
+def using_set(node):
+    """Removes duplicate nodes by building a set of values and re-pointing the next_node pointers. O(N) space and time
+
+        Args:
+            node: input Node
+        Returns:
+            node: with duplicates removed
+    """
+
+    all_data = set()
+    previous = None
+    while node is not None:
+        if node.data in all_data:
+            previous.next = node.next
+        else:
+            all_data.add(node.data)
+            previous = node
+
+        node = node.next
+
+    return node
+
+
+def using_pointers(node):
+    """Removes duplicate nodes by using 2 pointers including a runner which re-assigns nodes. O(N^2) time
+
+        Args:
+            node: input Node
+        Returns:
+            node: with duplicates removed
+    """
+
+    while node is not None:
+        runner = node
+
+        while runner.next is not None:
+            if runner.next.data == node.data:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+
+        node = node.next
---
+++ data_structures/chapter_2/test/test_remove_dups.py
@@ -0,0 +1,44 @@
+import unittest
+
+from data_structures.chapter_2.linked_list import Node
+from data_structures.chapter_2.questions.remove_dups import *
+
+
+def create_linked_list(data):
+    head = Node(data[0])
+    for datum in data[1:]:
+        head.append_to_tail(datum)
+
+    return head
+
+
+class TestRemoveDups(unittest.TestCase):
+    def remove_dups(self, method):
+        head = create_linked_list([1, 1, 2, 2, 3])
+
+        method(head)
+
+        self.assertEqual(head.data, 1)
+        self.assertEqual(head.next.data, 2)
+        self.assertEqual(head.next.next.data, 3)
+        self.assertIsNone(head.next.next.next)
+
+    def remove_dups_with_single_element(self, method):
+        head = create_linked_list([1])
+
+        method(head)
+
+        self.assertEqual(head.data, 1)
+        self.assertIsNone(head.next)
+
+    def test_remove_dups_using_set(self):
+        self.remove_dups(using_set)
+        self.remove_dups_with_single_element(using_set)
+
+    def test_remove_dups_using_pointers(self):
+        self.remove_dups(using_pointers)
+        self.remove_dups_with_single_element(using_pointers)
+
+
+if __name__ == "__main__":
+    unittest.main()
