26b4ba657de4d5877278321fcac4ea793141bb89
somethingvague <phil.j.regan@gmail.com>
2019-01-27 22:40:54 +0000

    Remove unnecessary power

--- data_structures/chapter_2/questions/sum_lists.py
+++ data_structures/chapter_2/questions/sum_lists.py
@@ -9,20 +9,18 @@ def _get_or_default(node):
     return Node(0) if node is None else node
 
 
-def _sum_lists(first, second, power, carry):
+def _sum_lists(first, second, carry):
     if first is None and second is None:
         return None
 
     first = _get_or_default(first)
     second = _get_or_default(second)
     value = carry + first.data + second.data
-    portion = (value % 10) * (10 ** power)
-
-    power += 1
+    portion = (value % 10)
     result = Node(portion)
-    result.next = _sum_lists(first.next, second.next, power, value >= 10)
+    result.next = _sum_lists(first.next, second.next, value >= 10)
     return result
 
 
 def sum_lists(first, second):
-    return _sum_lists(first, second, 0, 0)
+    return _sum_lists(first, second, 0)
