3b5dc12b37e9d0800c493532cb7fcedc84ae87ef
somethingvague <phil.j.regan@gmail.com>
2019-01-27 22:38:21 +0000

    Add solution for question 2.5

---
+++ data_structures/chapter_2/questions/sum_lists.py
@@ -0,0 +1,28 @@
+"""Question 2.5
+Write a function that sums 2 numbers represented by linked lists
+"""
+
+from data_structures.chapter_2.linked_list import Node
+
+
+def _get_or_default(node):
+    return Node(0) if node is None else node
+
+
+def _sum_lists(first, second, power, carry):
+    if first is None and second is None:
+        return None
+
+    first = _get_or_default(first)
+    second = _get_or_default(second)
+    value = carry + first.data + second.data
+    portion = (value % 10) * (10 ** power)
+
+    power += 1
+    result = Node(portion)
+    result.next = _sum_lists(first.next, second.next, power, value >= 10)
+    return result
+
+
+def sum_lists(first, second):
+    return _sum_lists(first, second, 0, 0)
---
+++ data_structures/chapter_2/test/test_sum_lists.py
@@ -0,0 +1,22 @@
+import unittest
+
+from data_structures.chapter_2.questions.sum_lists import sum_lists
+from utils.utils import create_linked_list
+
+
+class TestSumLists(unittest.TestCase):
+    def test_sum_lists_of_equal_length(self):
+        first = create_linked_list([7, 1, 6])
+        second = create_linked_list([5, 9, 2])
+
+        result = sum_lists(first, second)
+        expected = create_linked_list([2, 1, 9])
+        self.assertEqual(result, expected)
+
+    def test_sum_lists_of_unequal_length(self):
+        first = create_linked_list([7, 1])
+        second = create_linked_list([5, 9, 2])
+
+        result = sum_lists(first, second)
+        expected = create_linked_list([2, 1, 3])
+        self.assertEqual(result, expected)
